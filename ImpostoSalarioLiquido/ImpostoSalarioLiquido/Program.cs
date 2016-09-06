948a05a7a8ffe0ba45941a89f56a89d70b0d499b
Rodrigo Arnaldo de Araújo <rodrigo.a.araujo@hotmail.com>
2016-09-06 13:23:51 -0300

    Add files via upload

---
+++ ImpostoSalarioLiquido/ImpostoSalarioLiquido/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpostoSalarioLiquido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float salario, porcentagem, calcImposto;
+            
+            Console.WriteLine("Bem vindo ao calculo do imposto de salário.");
+            
+            Console.WriteLine("\nDigite o seu salário: ");
+            salario = float.Parse (Console.ReadLine());
+           
+            Console.WriteLine("\nDigite a porcentagem do imposto que será descontado.");
+            porcentagem = float.Parse(Console.ReadLine());
+            
+            if ((porcentagem < 0) || (porcentagem > 100))
+            {
+                Console.WriteLine("\nValor inválido, por favor digite valores de 0 a 100.");
+            }
+            else
+            {
+                calcImposto = (porcentagem * salario) / 100;
+                calcImposto = salario - calcImposto;
+                Console.WriteLine("\nO seu salário liquido com desconto de "+porcentagem+"%"+" é: {0:C}",calcImposto);
+                Console.WriteLine("\n\nPressione qualquer tecla para sair...");
+            }
+            
+
+            Console.ReadKey();
+
+
+            
+
+
+
+        }
+    }
+}
